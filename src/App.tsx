388bad0cfa73d91a59a5313612ba48ba924e4fec
aleattene <attene.tomorrowdevs@gmail.com>
2024-07-02 10:18:50 +0200

    feat: add routing (react-router-dom)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.tsx";
+import Sidebar from "./components/Sidebar/Sidebar.tsx";
+import Frontend from "./pages/it/Frontend/HomePage.tsx";
+import Backend from "./pages/it/Backend/HomePage.tsx";
+import Database from "./pages/it/Database/Database.tsx";
+import Docker from "./pages/it/Docker/Docker.tsx";
+import Git from "./pages/it/Git/Git.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AlgoSorting from "./pages/it/Algorithms/Sorting/AlgoSorting.tsx";
 
-function App() {
+const App: React.FC = () => {
 
-  return (
-    <>
-      <h1>Ale Attene - Knowledges</h1>
-    </>
-  )
+    const [section, setSection] = React.useState<string>('frontend');
+
+    const handleSectionChange = (newSection: string) => {
+        setSection(newSection);
+    }
+
+    return (
+        <BrowserRouter>
+            <div className="app">
+                <div className="header-container">
+                    <div className="navbar">
+                        <Navbar onSectionChange={handleSectionChange} />
+                    </div>
+                </div>
+                <div className="body-container">
+                    <div className="sidebar">
+                        <Sidebar section={section} />
+                    </div>
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/frontend" element={<Frontend/>} />
+                        <Route path="/backend" element={<Backend/>} />
+                        <Route path="/database" element={<Database/>} />
+                        <Route path="/docker" element={<Docker/>} />
+                        <Route path="/git" element={<Git/>} />
+                        <Route path="/algoritmi" element={<AlgoSorting/>} />
+                        <Route path="/" element={
+                            <div>
+                                <h1>Benvenuto!</h1>
+                                <p>Seleziona una sezione dalla navbar.</p>
+                            </div>
+                        } />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    </BrowserRouter>
+    );
 }
 
 export default App
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import './Navbar.css';
+import {Link, useLocation} from "react-router-dom";
 
 interface NavbarProps {
     onSectionChange: (section: string) => void;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ onSectionChange }) => {
+
+    const location = useLocation();
+
     return (
         <nav className="navbar">
             <ul>
-                <li onClick={() => onSectionChange('frontend')}>Frontend</li>
-                <li onClick={() => onSectionChange('backend')}>Backend</li>
-                <li onClick={() => onSectionChange('database')}>Database</li>
-                <li onClick={() => onSectionChange('docker')}>Docker</li>
-                <li onClick={() => onSectionChange('git')}>Git</li>
+
+                <li
+                    className={location.pathname === '/frontend' ? 'active' : ''}
+                    onClick={() => onSectionChange('frontend')}
+                >
+                    <Link to="/frontend">Frontend</Link>
+                </li>
+                <li
+                    className={location.pathname === '/backend' ? 'active' : ''}
+                    onClick={() => onSectionChange('backend')}
+                >
+                    <Link to="/backend">Backend</Link>
+                </li>
+                <li
+                    className={location.pathname === '/database' ? 'active' : ''}
+                    onClick={() => onSectionChange('database')}
+                >
+                    <Link to="/database">Database</Link>
+                </li>
+                <li
+                    className={location.pathname === '/docker' ? 'active' : ''}
+                    onClick={() => onSectionChange('docker')}
+                >
+                    <Link to="/docker">Docker</Link>
+                </li>
+                <li
+                    className={location.pathname === '/git' ? 'active' : ''}
+                    onClick={() => onSectionChange('git')}
+                >
+                    <Link to="/git">Git</Link>
+                </li>
+                <li
+                    className={location.pathname === '/algoritmi' ? 'active' : ''}
+                    onClick={() => onSectionChange('algoritmi')}
+                >
+                    <Link to="/algoritmi">Algoritmi</Link>
+                </li>
             </ul>
         </nav>
     );
