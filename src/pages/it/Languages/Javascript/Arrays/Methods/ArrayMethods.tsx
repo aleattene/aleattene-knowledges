694600b886d48e822fc6746c3f00791c146c2b74
aleattene <attene.tomorrowdevs@gmail.com>
2024-07-07 12:32:55 +0200

    feat(js-array): add copyWithin method

--- src/pages/it/Languages/Javascript/Arrays/Methods/ArrayMethods.tsx
+++ src/pages/it/Languages/Javascript/Arrays/Methods/ArrayMethods.tsx
@@ -42,6 +42,48 @@ const ArrayMethods: React.FC = () => {
                 console.log(allFruits)      // ['apple', 'banana', 'cherry', 'orange']
             `}/>
 
+
+            <h3> Metodo
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/copyWithin"
+                   target="_blank" rel="noopener noreferrer">
+                    <code className='documentation-link'> copyWithin </code>
+                </a>
+            </h3>
+            <p> Questo metodo copia superficialmente parte di un array (porzione di elementi)
+                in un'altra posizione nello stesso array e restituisce l'array modificato
+                senza modificarne la lunghezza.
+            </p>
+            <h2>Sintassi:</h2>
+            <JavascriptCode code={`array.copyWithin(target, start, end)`}/>
+            <p>dove:
+                <ul>
+                    <li><code>target</code> è l'indice iniziale in cui iniziare a copiare gli elementi
+                                            (se l'indice è negativo fa riferimento alla fine dell'array)</li>
+                    <li><code>start</code> (opzionale) è l'indice iniziale da cui selezionare/copiare gli elementi
+                                           (se non specificato di default ha valore zero mentre se ha valore negativo
+                                            fa riferimento alla fine dell'array)</li>
+                    <li><code>end</code> (opzionale) è l'indice finale (escluso) di selezione/copia degli elementi
+                                         (se non specificato di default è la lunghezza dell'aray mentre se ha valore
+                                            negativo fa riferimento alla fine dell'array)
+                    </li>
+                </ul>
+            </p>
+            <h3>Esempio:</h3>
+            <JavascriptCode code={
+                `const array = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+                 array.copyWithin(2, 0, 3);
+
+                 console.log(array);  // Output: ['apple', 'banana', 'apple', 'banana', 'cherry']
+            `}/>
+            <p>
+                In questo esempio, il metodo <code>copyWithin</code> copia gli elementi dall'indice 0 all'indice 3
+                (escluso) e li incolla a partire dall'indice 2 nello stesso array (sovrascrivendo i valori)
+                senza quindi restituire un nuovo array.
+            </p>
+            <p>
+                Si tratta di un metodo utile quando si desidera effettuare copie e sovrascritture di elementi
+                all'interno dello stesso array, senza la necessita di crearne uno nuovo.
+            </p>
         </div>
     );
 };
