646b4bb65e30c8fc885b8cba3112692397bd9066
aleattene <attene.tomorrowdevs@gmail.com>
2024-07-29 11:16:46 +0200

    feat(http): add http methods list and description

--- src/pages/it/HTTP/HttpMethods/HttpMethods.tsx
+++ src/pages/it/HTTP/HttpMethods/HttpMethods.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import './HttpMethods.css'
 
-const httpRequestMethods = [
-    'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT'
-]
+interface httpRequestMethods {
+    [key: string]: string;
+}
+
+const httpRequestMethods: httpRequestMethods = {
+    'GET': 'Trasferisce una rappresentazione della risorsa richiesta.',
+    'HEAD': 'Simile a GET, ma non trasferisce il contenuto della risposta.',
+    'POST': 'Processa la risorsa usando il contenuto della richiesta.',
+    'PUT': 'Sostituisce la risorsa indicata con il contenuto della richiesta.',
+    'PATCH': 'Modifica la risorsa indicata con il contenuto della richiesta.',
+    'DELETE': 'Rimuove tutte le rappresentazioni della risorsa indicata.',
+    'CONNECT': 'Crea un tunnel verso il server identificato dal risorsa indicata.',
+    'OPTIONS': 'Descrive le possibili opzioni per comunicare con la risorsa indicata.',
+    'TRACE': 'Mostra se la risorsa viene modificata durante la comunicazione.'
+};
+
 
 const HttpRequestMethods: React.FC = () => {
     return (
         <div>
-            <h1>HTTP Request Methods</h1>
+            <h1>Metodi HTTP</h1>
+            <p>I metodi HTTP indicano l'azione che si desidera eseguire sulla risorsa indicata nella richiesta:</p>
             <ul>
-                { httpRequestMethods.map(((method, index) => (
-                    <li key={index}> {method} </li>
-                )))}
+                {Object.entries(httpRequestMethods).map(([key, value]) => {
+                    return <li> <b>{key}</b>: {value} </li>
+                })}
             </ul>
-
         </div>
     );
 };
