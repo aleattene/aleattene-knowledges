009778a047945a073ebbca3d1b93dca2fc777499
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-03 22:52:22 +0200

    feat(git): add alias commands

--- src/pages/it/Git/Git.tsx
+++ src/pages/it/Git/Git.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TerminalCode from "../../../components/Code/TerminalCode/TerminalCode.tsx";
 
-//
+// INIZIALIZZAZIONE
 const createEmptyLocalRepository: string = `git init`;
 const cloneRepository: string = `git clone url_repository.git`;
 // UPDATE
-const getUpdatesNoAplly: string = `git fetch`;
-const getUpdatesAplly: string = `git pull`;
+const getUpdatesNoApply: string = `git fetch`;
+const getUpdatesApply: string = `git pull`;
 // COMMIT
 const addFileToStage: string = `git add path_file`;
 const commitChanges: string = `git commit -m "message"`;
@@ -23,6 +23,8 @@ const createSwitchBranch: string = `git checkout -b branch_name`;
 const mergeBranch: string = `git merge branch_name`;
 const deleteBranch: string = `git branch -d branch_name`;
 const branchList: string = `git branch`;
+const branchListRemote: string = `git branch -r`;
+
 // HISTORY
 const history: string = `git log`;
 const historyGraph: string = `git log --graph --oneline --all`;
@@ -37,16 +39,15 @@ const Git: React.FC = () => {
     return (
         <div>
             <h1>Git</h1>
-
             <h3>Creazione di un Nuovo Repository Locale</h3>
             <TerminalCode code={createEmptyLocalRepository}/>
             <h3>Clonare in Locale un Repository Remoto</h3>
             <TerminalCode code={cloneRepository}/>
 
             <h3>Verifica se ci sono aggiornamenti Remoti</h3>
-            <TerminalCode code={getUpdatesNoAplly}/>
+            <TerminalCode code={getUpdatesNoApply}/>
             <h3>Applica Localmente gli Aggiornamenti Remoti</h3>
-            <TerminalCode code={getUpdatesAplly}/>
+            <TerminalCode code={getUpdatesApply}/>
             <h3>Aggiunta File(s) all'area di Staging</h3>
             <TerminalCode code={addFileToStage}/>
             <h3>Commit delle modifiche (snapshot stato corrente progetto)</h3>
@@ -70,6 +71,8 @@ const Git: React.FC = () => {
             <TerminalCode code={deleteBranch}/>
             <h3>Visualizzare l'elenco dei Branch Locali</h3>
             <TerminalCode code={branchList}/>
+            <h3>Visualizzare l'elenco dei Branch Remoti</h3>
+            <TerminalCode code={branchListRemote}/>
 
             <h3>Visualizza la History dei Commit</h3>
             <TerminalCode code={history}/>
@@ -81,6 +84,40 @@ const Git: React.FC = () => {
             <TerminalCode code={diffCached}/>
             <h3>Visualizza la differenza tra 2 commit</h3>
             <TerminalCode code={diffCommits}/>
+
+            <h2>Alias</h2>
+            <p>Gli alias sono dei comandi personalizzati che consentono la creazione di abbreviazioni/sinonimi per
+                comandi Git frequentemente utilizzati più lunghi e complessi.
+                Il loro uso diventa quindi particolarmente utile per velocizzare il lavoro quotidiano.
+                Per creare un alias è sufficiente aggiungere la seguente riga al file <code>.gitconfig</code> presente
+                nella home directory:
+            </p>
+            <TerminalCode code={`
+                [alias]
+                    alias_name = command
+            `}/>
+            <p>che in maniera più specifica:</p>
+            <TerminalCode code={`
+                [alias]
+                    co = checkout
+                    ci = commit
+                    br = branch
+                    st = status
+            `}/>
+            <p>Quindi una volta che andremo ad utilizzare Git potremo scrivere:</p>
+            <TerminalCode code={`
+                // git checkout branch_name
+                git co branch_name 
+                
+                // git commit -m "message"
+                git ci -m "message"
+                
+                // git branch branch_name
+                git br branch_name
+                
+                // git status
+                git st 
+            `}/>
         </div>
     );
 };
