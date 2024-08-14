ffdc2372dcc9359f8ab523583b97fd696641ccce
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-14 12:14:28 +0200

    feat(frontend): add main web breakpoints

--- src/pages/it/Frontend/Frontend.tsx
+++ src/pages/it/Frontend/Frontend.tsx
@@ -4,7 +4,15 @@ const Frontend: React.FC = () => {
     return (
         <div>
             <h1>Frontend</h1>
-            <p>Contenuti relativi al Frontend.</p>
+            <h2>Web Breakpoints</h2>
+            <ul>
+                <li>Mobile: &lt;= 480px [TO FIX img]</li>
+                <li>Mobile Landscape: &lt;= 768px [TO FIX img]</li>
+                <li>Tablet: &lt;= 834px [TO FIX img]</li>
+                <li>Tablet Landscape: &lt;= 1024px [TO FIX img]</li>
+                <li>Laptop: &lt;= 1440px [TO FIX img]</li>
+                <li>Desktop: &lt;= 1440px [TO FIX img]</li>
+            </ul>
         </div>
     );
 };
