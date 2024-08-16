d0085cb5cf041cd6ebe8f1d3babe55e43acb66ed
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-16 17:31:40 +0200

    feat(python): add lists examples

--- src/pages/it/Languages/Python/Lists/ListPy.tsx
+++ src/pages/it/Languages/Python/Lists/ListPy.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ListPy.css'
 import Table from "../../../../../components/Table/Table.tsx";
+import PythonCode from "../../../../../components/Code/PythonCode/PythonCode.tsx";
 
 const pythonListMethods = [
     { Method: 'append()', Description: 'Aggiunge un elemento alla fine della lista.', Example: 'my_list.append(4)' },
@@ -20,6 +21,62 @@ const ListPy: React.FC = () => {
     return (
         <div>
             <h1>Lista Python</h1>
+            <p>Le liste sono istanze della classe List.</p>
+            <p>Esempi di utilizzo:</p>
+            <PythonCode code={`
+                # Inizializzazione
+                my_list = []                # monodimensionale
+                my_list = [1, 2, 3]
+                my_list = list()            # costruttore della classe List
+                my_list = [[1, 2], [3, 4]]  # bidimensionale (liste innestate)
+            `}/>
+            <p>[TO FIX] Slicing: possibile utilizzarlo sia per le liste che per le stringhe.</p>
+            <PythonCode code={`
+                # Inserimento nuovo elemento (metodo append della classe List) in fondo alla lista
+                my_list = [10, 20, 30]
+                my_list.append(50)      # [10, 20, 30, 50]
+                
+                # Inserimento nuovo elemento (metodo insert della classe List) in posizione specifica (indice)
+                my_list.insert(3, 40)   # [10, 20, 30, 40, 50]
+            `}/>
+            <PythonCode code={`
+                # Cancellare un elemento
+                my_list = [10, 20, 30]
+                del my_list(1)          # [10, 30]
+            `}/>
+            <PythonCode code={`
+                # Operatore in (iterare tra gli elementi all'interno di una lista)
+                my_list = [10, 20, 30]
+                20 in my_list       # True (boolean)
+                15 in my_list       # False
+            `}/>
+            <PythonCode code={`
+                # Due Nomi per un'unica lista
+                my_list = [10, 20, 30]
+                
+                my_list2 = mylist       # copia per referenza (entrambe puntano allo stesso indirizzo di memoria)
+                
+                my_list2[1] = 60        # modifico valore secondo elemento
+                
+                print(my_list2)         # Output: [10, 60, 30]
+                print(my_list)          # Output: [10, 60, 30]
+            `}/>
+            <PythonCode code={`
+                # Copia di una lista (con funzione copy)
+                my_list = [10, 20, 30]
+                
+                my_list2 = my_list.copy()        # duplicazione lista
+                my_list2[1] = 60                 # modifica elemento
+                 
+                print(my_list2)     # Output: [10, 60, 30] modificata solo questa lista
+                print(my_list)      # Output: [10, 20, 30] lista originale non modificata
+            `}/>
+
+
+
+
+
+
             <h2>Metodi delle Liste</h2>
             <Table headers={['Metodo', 'Descrizione', 'Esempio']} data={pythonListMethods}/>
         </div>
