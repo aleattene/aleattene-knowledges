6383c778fa90717642d4fa097107cf33d4d43b56
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-16 17:31:32 +0200

    feat(python): add dictionaries examples

--- src/pages/it/Languages/Python/Dictionaries/DictionaryPy.tsx
+++ src/pages/it/Languages/Python/Dictionaries/DictionaryPy.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DictionaryPy.css'
 import Table from "../../../../../components/Table/Table.tsx";
+import PythonCode from "../../../../../components/Code/PythonCode/PythonCode.tsx";
 
 
 const pythonDictMethods = [
@@ -22,6 +23,100 @@ const DictionaryPy: React.FC = () => {
     return (
         <div>
             <h1>Dizionario Python</h1>
+            <p>I dizionari, rispetto alle liste ed alle tuple, non hanno un ordinamento definito degli elementi.
+                Essi sono composti da coppi <code>chiave: valore</code> e sono istanze della classe <code>Dict</code>.
+                Per accedere al valore di un elemento, non essendo presente un ordinamento (quindi nessun indice),
+                si utilizza la chiave (che deve essere univoca, di qualunque tipo ed immutabile) associata al valore.
+            </p>
+            <h3>Esempi di utilizzo:</h3>
+            <PythonCode code={`
+                # Creazione di un dizionario vuoto
+                my_dict = {}
+                my_dict = dict()
+                
+                # Inizializzazione di un dizionario con valori
+                my_dict = { 'primo': 10 , 'secondo': 20 }
+                
+                # Modifica valore di un elemento (sintassi ed esempio)
+                dizionario[chiave_presente] = nuovo_valore
+                
+                my_dict['primo'] = 0
+                print(my_dict)          # { 'primo': 0 , 'secondo': 20 }
+                
+                # Aggiunta di un elemento (sintassi ed esempio)
+                dizionario[chiave_non_presente] = valore
+                
+                my_dict['terzo'] = 30
+                print(my_dict)      # { 'primo': 0 , 'secondo': 20, 'terzo': 30 }
+                
+                # Cancellazione di un elemento (sintassi ed esempio)
+                del dizionario[chiave_presente]
+                
+                del my_dict['primo']
+                print(my_dict)      # { 'secondo': 20, 'terzo': 30 }
+                
+                # Svuotamento dizionario (cancellando tutte le chiavi e valori)
+                my_dict.clear()
+                my_dict = {}
+            `}/>
+            <p>Quando si vuole verificare la presenza o meno di una chiave in un dizionario si può usare l'operatore
+                <a href={'#'}>
+                    <code className={'documentation-link'} > in </code>
+                </a>:
+            </p>
+            <PythonCode code={`
+                my_dict = { 'primo': 10 , 'secondo': 20 }
+                'primo' in my_dict       # True
+                'decimo' in my_dict       # False
+            `}/>
+            <p>Molto interessante ed utile la possibilità di copia di un dizionario:</p>
+            <PythonCode code={`
+                my_dict = { 'primo': 10, 'secondo': 20 }
+                
+                my_dict2 = my_dict          # puntano entrambi allo stesso indirizzo di memoria 
+                
+                print(my_dict)      # { 'primo': 10 , 'secondo': 20 }
+                print(my_dict2)     # { 'primo': 10 , 'secondo': 20 }
+                
+                my_dict2 = my_dict.copy()   # puntano a due indirizzi/oggetti differenti
+                
+                # Modifica di un valore in my_dict
+                my_dict['primo'] = 0
+                
+                print(my_dict)      # { 'primo': 0 , 'secondo': 20 }
+                print(my_dict2) # { 'primo': 10 , 'secondo': 20 }
+                
+                # Modifica di un valore in my_dict2
+                my_dict2['secondo'] = 0
+                
+                print(my_dict) # { 'primo': 0 , 'secondo': 20 }
+                print(my_dict2) # { 'primo': 10 , 'secondo': 0 }
+            `}/>
+            <h3>Metodo Items</h3>
+            <p>Il metodo <code>items()</code> ritorna una lista contenente una tupla per ogni coppia chiave-valore.</p>
+            <PythonCode code={`
+                my_dict1 = { 10: 'a', 20: 'b'}
+                my_dict2 = { 30: 'c' }
+                
+                list1 = my_dict1.items()        # riferimento ad oggetto di tipo dict-items (iteratore)
+                list2 = my_dict2.items()        
+                
+                print(list1)        # [ (10, 'a'), (20, 'b')]
+                print(list2)        # [ (30, 'c') ]
+                
+                # Possibile conversione (da lista di tuple a dizionario)
+                my_dict3 = dict(list1)      # { 10: 'a', 20: 'b'}
+                
+                # Possibili ulteriori modifiche
+                my_dict3.update(dict(list2))       # { 10: 'a', 20: 'b', 30: 'c'}
+            `}/>
+            <p><i>In questo ultimo caso abbiamo aggiornato il dizionario my_dict3 aggiungendo ad esso le coppie
+                chiave-valore del dizionario ottenuto trasformando la lista di tuple list2 in un dizionario.</i>
+            </p>
+
+
+
+
             <h2>Metodi dei Dizionari</h2>
             <Table headers={['Metodo', 'Descrizione', 'Esempio']} data={pythonDictMethods}/>
         </div>
