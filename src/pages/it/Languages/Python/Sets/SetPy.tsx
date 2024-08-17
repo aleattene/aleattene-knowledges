14e70e63751e5dbcf658e1ff187d4435140af53b
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-17 09:40:43 +0200

    feat(python): add sets examples

--- src/pages/it/Languages/Python/Sets/SetPy.tsx
+++ src/pages/it/Languages/Python/Sets/SetPy.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SetPy.css'
 import Table from "../../../../../components/Table/Table.tsx";
+import PythonCode from "../../../../../components/Code/PythonCode/PythonCode.tsx";
 
 const pythonSetMethods = [
     { Method: 'add()', Description: 'Aggiunge un elemento al set.', Example: 'my_set.add(1)' },
@@ -20,6 +21,50 @@ const SetPy: React.FC = () => {
     return (
         <div>
             <h1>Set Python</h1>
+            <p>I set (insiemi) sono oggetti della classe <code>Set</code> e vengono utilizzati come contenitore di
+                elementi univoci. Sono simili ai dizionari, dai quali però sono eliminati tutti i valori e mantenute
+                solamente le chiavi.</p>
+            <p>Esempi di utilizzo:</p>
+            <PythonCode code={`
+                my_set = set()
+                my_set = set([10,20,30,40]) # lista-parametro del costruttore di Set
+                my_set = {10, 20, 30, 40} # literal form
+                # my_set = {} NO -> creerebbe un dizionario vuoto, non un insieme vuoto
+                my_set.add(50) # {10, 20, 30, 40, 50}
+            `}/>
+            <p>Interessante osservazione di può fare con i <code>frozenset</code> che sono set immutabili.</p>
+            <PythonCode code={`
+                my_set = frozenset([10,20,30,40])
+                my_set.add(50)      # AttributeError: 'frozenset' object has no attribute 'add'
+            `}/>
+            <p>L'errore appena ottenuto deriva dal fatto che <code>add()</code> è un metodo della classe
+                <code>Set</code> e non di <code>Frozenset</code>.</p>
+            <p>Anche per i set è possibile utilizzare l'operatore <code>in</code> :</p>
+            <PythonCode code={`
+                my_set = {10, 20, 30, 40}
+                10 in my_set    # True
+                90 in my_set    # False
+            `}/>
+            <p>Ancora più interessanti sono però gli operatori insiemistici:</p>
+            <PythonCode code={`
+                # Dichiarazione Set
+                my_set1 = {10, 20, 30, 40}
+                my_set2 = {30, 40, 50, 60}
+                
+                # Intersezione (&) : ritorna gli elementi comuni ai due set
+                set_intersection = my_set1 & my_set2    # {30, 40}
+                
+                # Unione (|) : ritorna un set contenente tutti gli elementi di entrambi i set (senza duplicati)
+                set_union = my_set1 | my_set2            # {10, 20, 30, 40, 50, 60}
+                
+                # Differenza (-) : ritorna un set contenente gli elementi appartenenti al primo set ma non al secondo
+                set_difference1 = my_set1 - my_set2        # { 10, 20 }
+                set_difference2 = my_set2 - my_set1        # { 50, 60 }
+                
+                # OR esclusivo (^) : ritorna un set contenente gli elementi che non sono comuni ai due set
+                set_symmetric_difference = my_set1 ^ my_set2    # {10, 20, 50, 60}
+            `}/>
+
             <h2>Metodi dei Set</h2>
             <Table headers={['Metodo', 'Descrizione', 'Esempio']} data={pythonSetMethods}/>
         </div>
