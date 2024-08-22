a2f21b13c9b497bd3f7e2bc23de25dde8984e9b7
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-22 10:32:56 +0200

    feat(fe/html): add aria-labelledby attribute

--- src/pages/it/Frontend/Html/ARIA/Aria.tsx
+++ src/pages/it/Frontend/Html/ARIA/Aria.tsx
@@ -33,6 +33,26 @@ const Aria: React.FC = () => {
                     </tr>
                 </table>
             `}/>
+
+            <h2>Attributo aria-labelledby</h2>
+            <p>L'attributo
+                <a href={"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-labelledby"}>
+                    <code className={'documentation-link'}> aria-labelledby </code>
+                </a>
+                è utilizzato per indicare agli strumenti di accessibilità (come i lettori di schermo) quale elemento
+                o elementi del DOM (Document Object Model) forniscono l'etichetta o il nome per l'elemento corrente.
+            </p>
+            <p>L'attributo accetta un elenco di ID di elementi separati da spazi, e gli strumenti di accessibilità
+                leggeranno il contenuto di quegli elementi come l'etichetta dell'elemento corrente.
+            </p>
+            <p>Ad esempio, potremmo avere un modulo con un campo di input e un'etichetta separata, e potremmo
+                utilizzare <code>aria-labelledby</code> per collegare l'etichetta all'input, nel modo seguente:
+            </p>
+            <JavascriptCode code={`
+                <\label id="name-label">Nome:</label>
+                <input type="text" aria-labelledby="name-label" />
+            `}/>
+            <p>Così facendo lo strumento di accessibilità leggerà "Nome:" come l'etichetta per il campo di input.</p>
         </div>
     );
 }
