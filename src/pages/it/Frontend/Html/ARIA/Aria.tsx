cb1ae35b916876394f2cdf3575ee2a4af18efb55
aleattene <attene.tomorrowdevs@gmail.com>
2024-08-22 10:53:04 +0200

    feat(fe/html): add aria description and example

--- src/pages/it/Frontend/Html/ARIA/Aria.tsx
+++ src/pages/it/Frontend/Html/ARIA/Aria.tsx
@@ -7,8 +7,63 @@ const Aria: React.FC = () => {
             <h1>ARIA</h1>
             <p>ARIA (Accessible Rich Internet Applications) è un insieme di attributi che possono essere aggiunti agli
                 elementi HTML per fornire informazioni aggiuntive su come questi elementi dovrebbero essere
-                interpretati.
+                interpretati, in particolar modo per rendere più accessibile e fruibile il web a quei particolari
+                utenti che sono costretti ad utilizzare tecnologie assistive (come i lettori di schermo) per potervi
+                accedere.
             </p>
+            <p>Alcuni attributi ARIA comuni sono:</p>
+            <ul>
+                <li><code>Ruoli ARIA</code>: definiscono il tipo di elemento (ad esempio un pulsante, un menu, una
+                    finestra di dialogo, ecc.)
+                    <JavascriptCode code={`
+                        role="navigation"   // per un menu di navigazione
+                        role="button"       // per un pulsante
+                        role="dialog"       // per una finestra di dialogo 
+                    `}/>
+                </li>
+                <li><code>Proprietà ARIA</code>: forniscono informazioni aggiuntive che possono essere utili agli utenti
+                    (ad esempio per indicare che un campo di input è obbligatorio, ecc.)
+                    <JavascriptCode code={`
+                        aria-required="true"    // per indicare che un campo di input è obbligatorio
+                    `}/>
+                </li>
+                <li><code>Stati ARIA</code>: forniscono informazioni sullo stato corrente di un elemento (ad esempio se è
+                    espanso o compresso, se è disabilitato, ecc.) e come cambia a seguito di interazioni con l'utente
+                    <JavascriptCode code={`
+                        aria-checked="true"     // per indicare che una casella di controllo è selezionato             
+                        aria-expanded="true"    // per indicare che un elemento è espanso
+                        aria-disabled="true"    // per indicare che un elemento è disabilitato
+                    `}/>
+                </li>
+            </ul>
+            <h3>Esempio:</h3>
+            <JavascriptCode code={`
+                <nav role="navigation">
+                    <ul>
+                        <li><a href="#home" aria-label="Vai alla home page">Home</a></li>
+                        <li><a href="#about" aria-label="Vai alla pagina Chi siamo">Chi siamo</a></li>
+                        <li><a href="#contact" aria-label="Vai alla pagina Contatti">Contatti</a></li>
+                    </ul>
+                </nav>
+            `}/>
+            <p>In questo esempio possiamo osservare che:
+                <ul>
+                    <li>l'attributo <code>role="navigation"</code> aiuta le tecnologie assistive a capire che
+                        l'elemento <code>&lt;nav&gt;</code> è usato per la navigazione
+                    </li>
+                    <li>l'attributo  <code>aria-label</code> fornisce una descrizione dettagliata di ogni link,
+                        che può essere letta ad alta voce da un lettore di schermo.
+                    </li>
+                </ul>
+            </p>
+            <p><i>Piccola nota a margine di fondamentale importanza è quella di non utilizzare ARIA per correggere
+                problemi di accessibilità che possono essere risolti con HTML e CSS standard.
+                ARIA dovrebbe essere utilizzato solo come ultima risorsa, quando i soli strumenti standard non sono
+                sufficienti a garantire l'accessibilità appropriata ad un sito web.
+                A riguardo è infatti importantissimo seguire le linee guida di accessibilità, ovvero le WCAG
+                (Web Content Accessibility Guidelines).</i>
+            </p>
+
             <h2>Attributo Role</h2>
             <p>L'attributo
                 <a href={"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles"}>
@@ -27,7 +82,7 @@ const Aria: React.FC = () => {
             </p>
             <h3>Esempio</h3>
             <JavascriptCode code={`
-                <\table role="presentation">
+                <table role="presentation">
                     <tr>
                         <td>Questo è solo un testo di formattazione.</td>
                     </tr>
