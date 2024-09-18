77dd5773e29353736bd45500dad0806e01fee9c5
aleattene <attene.tomorrowdevs@gmail.com>
2024-09-18 10:59:25 +0200

    feat(JS/events): add preventDefault and stopPropagation descriptions and examples

--- src/pages/it/Languages/Javascript/Events/EventJS.tsx
+++ src/pages/it/Languages/Javascript/Events/EventJS.tsx
@@ -129,6 +129,131 @@ const EventJS: React.FC = () => {
                 }
                 ...
             `}/>
+
+            <h3>Prevenire il comportamento predefinito di un evento (preventDefault)</h3>
+            <p>Ogni evento ha un suo comportamento predefinito (ad esempio, se si clicca su un link, il
+                comportamento predefinito è quello di aprire il link).
+            </p>
+            <p>Ecco allora che per prevenire il comportamento predefinito di un evento si può utilizzare il metodo
+                preventDefault(). Ad esempio:
+            </p>
+            <JavascriptCode code={`
+                // File HTML
+                <\form action="">
+                    <label for="myInput">Nome:</label>
+                    <input id="myInput" type="text">
+                    <button type="submit">Invia</button>
+                </form>
+            `}/>
+            <JavascriptCode code={`
+                // File JavaScript
+                const form = document.querySelector('form');
+                form.addEventListener('submit', (event) => {
+                    // previene il comportamento predefinito di refresh pagina al submit
+                    event.preventDefault(); 
+                    // comportamento custom al submit
+                    console.log('Form not submitted!');
+                });
+            `}/>
+
+            <h3>Prevenire la Propagazione degli eventi (stopPropagation)</h3>
+            <p>Quando si clicca su un elemento, l'evento si propaga in tutto il DOM. Esiste infatti un flusso di
+                propagazione degli eventi che va dal nodo più interno al nodo più esterno.
+            </p>
+            {/* [TO COMPLETE] IMG flusso propagazione eventi */}
+            <p>Questo flusso di propagazione si compone sostanzialmente di tre fasi:</p>
+            <ul>
+                <li>La fase di cattura (capturing phase): l'evento scende dal nodo più esterno al nodo più interno</li>
+                <li>La fase di target (target phase): l'evento arriva al nodo target</li>
+                <li>La fase di bubbling (bubbling phase): l'evento risale dal nodo più interno al nodo più esterno</li>
+            </ul>
+            <p>Per prevenire la propagazione degli eventi (come sarebbe previsto di default) si può utilizzare il
+                metodo stopPropagation(). Ad esempio:
+            </p>
+            <JavascriptCode code={`
+                // File HTML
+                <\div id="primoDiv">
+                    <p id="primoP">Clicca qui ... </div>
+                    <p id="secondoP">Clicca anche qui ...</div>
+                </div>
+            `}/>
+            <JavascriptCode code={`
+                // File JavaScript
+                const primoDiv = document.getElementById('primoDiv');
+                primoDiv.addEventListener('click', (event) => {
+                    console.log('Primo Div cliccato!');
+                });
+                
+                const primoP = document.getElementById('primoP');
+                primoP.addEventListener('click', (event) => {
+                    console.log('Primo P cliccato!');
+                });
+                
+                const secondoP = document.getElementById('secondoP');
+                secondoP.addEventListener('click', (event) => {
+                    console.log('Secondo P cliccato!');
+                });
+            `}/>
+            <p>Cliccando sul primo paragrafo otterremo quanto segue:</p>
+            <TerminalCode code={`
+                Primo P cliccato!
+                Primo Div cliccato!
+            `}/>
+            <p>Succede questo perché la cattura dell'evento avviene durante la fase di bubbling (risalita), quindi
+                il primo a rilevare l'evento è il paragrafo (più interno), poi propagandosi viene rilevato anche dal
+                div (più esterno).
+            </p>
+            <p>Qualora volessimo invertire l'ordine di captura dell'evento, si può utilizzare il terzo parametro
+                del metodo addEventListener() impostandolo a true (di default è false):
+            </p>
+            <JavascriptCode code={`
+                const primoDiv = document.getElementById('primoDiv');
+                primoDiv.addEventListener('click', (event) => {
+                    console.log('Primo Div cliccato!');
+                }, true);
+                
+                const primoP = document.getElementById('primoP');
+                primoP.addEventListener('click', (event) => {
+                    console.log('Primo P cliccato!');
+                }, true);
+                
+                const secondoP = document.getElementById('secondoP');
+                secondoP.addEventListener('click', (event) => {
+                    console.log('Secondo P cliccato!');
+                }, true);
+            `}/>
+            <p>Il risultato sarà il seguente:</p>
+            <TerminalCode code={`
+                Primo Div cliccato!
+                Primo P cliccato!
+            `}/>
+            <p>Se infine volessimo prevenire la propagazione dell'evento dal paragrafo al div, si può utilizzare il
+                metodo stopPropagation():
+            </p>
+            <JavascriptCode code={`
+                const primoDiv = document.getElementById('primoDiv');
+                primoDiv.addEventListener('click', (event) => {
+                    console.log('Primo Div cliccato!');
+                });
+            
+                const primoP = document.getElementById('primoP');
+                primoP.addEventListener('click', (event) => {
+                    event.stopPropagation();
+                    console.log('Primo P cliccato!');
+                });
+                
+                const secondoP = document.getElementById('secondoP');
+                secondoP.addEventListener('click', (event) => {
+                    console.log('Secondo P cliccato!');
+                });
+            `}/>
+            <p>In tal caso l'evento sarà catturato solo dal paragrafo, senza propagarsi al div:</p>
+            <TerminalCode code={`
+                Primo P cliccato!
+            `}/>
+
+
+
         </div>
     );
 };
