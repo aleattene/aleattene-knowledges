b078cc16acada3d6554a93c33e6f3dc2feb3dc42
aleattene <attene.tomorrowdevs@gmail.com>
2024-10-17 10:59:12 +0200

    feat(django): add django-versions and pattern MVC/MTV descriptions and examples

--- src/pages/it/Backend/Django/DjangoBE.tsx
+++ src/pages/it/Backend/Django/DjangoBE.tsx
@@ -99,6 +99,205 @@ const DjangoBE: React.FC = () => {
                     </body>
                 </html>
             `}/>
+            <h2>Versioni di Django</h2>
+            <p>Le versioni di Django seguono uno schema di numerazione ben definito, dove la prima cifra indica la
+                versione principale, la seconda cifra indica la versione minore e la terza cifra indica la patch di
+                correzione. Ad esempio, la versione 4.1.1 indica che si tratta della versione 4 di Django, con la patch
+                di correzione 1.
+                Tra le versioni tra cui scegliere un'importante distinzione va fatta tra:
+                <ul>
+                    <li>versioni LTS (Long Term Support): versioni supportate a lungo termine, con un ciclo di vita di
+                        circa 3 anni, che ricevono aggiornamenti di sicurezza e correzioni di bug per un periodo di
+                        tempo più lungo rispetto alle versioni non LTS. Vale la pena scegliere questa versione quando si
+                        ha a che fare con progetti che richiedono un elevato livello di affidabilità ed un ciclo di vita
+                        di diversi anni.
+                    </li>
+                    <li>versioni non LTS: versioni che ricevono aggiornamenti di sicurezza e correzioni di bug per un
+                        periodo di tempo più breve rispetto alle versioni LTS. Si tratta quindi di scegliere queste
+                        versioni nel momento in cui si affrontano progetti che necessitano delle ultime funzionalità pur
+                        essendo disposti ad accettare un rischio maggiore di instabilità.
+                    </li>
+                </ul>
+            </p>
+            <h2>Architettura Django</h2>
+            <p>Django segue il modello architetturale MTV (Model-Template-View) che è una variante del più noto MVC
+                (Model-View-Controller). Si tratta di un'architettura organizzativa chiara per lo sviluppo di
+                applicazioni web che separa:
+                <ul>
+                    <li>la logica di business (Model)</li>
+                    <li>la presentazione dei dati (View)</li>
+                    <li>la gestione delle interazioni utente (Template)</li>
+                </ul>
+            </p>
+            <h2>MVC vs MTV</h2>
+            <p>Le architetture MVC basano la suddivisione delle applicazioni in tre componenti distinti, ognuno dei
+                quali con un ruolo specifico:
+                <ul>
+                    <li>Model: rappresenta la struttura dei dati e gestisce la logica di business; in sostanza il Model
+                        definisce come i dati sono organizzati, elaborati e archiviati. Nel caso di Django il Model è
+                        responsabile di definire la struttura del database, interagendo con esso attraverso il proprio
+                        ORM (Object-Relational Mapping).
+                    </li>
+                    <li>View: gestisce la presentazione dei dati all'utente esibendo i dati forniti dal Model ma in un
+                        formato più adatto. Nel caso di Django, con la variante MTV, la View può essere paragonata alla
+                        componente Template, responsabile quindi di definire come i dati devono essere presentati
+                        all'utente.
+                    </li>
+                    <li>Controller: gestisce le interazioni utente tra Model e View, occupandosi infatti di ricevere le
+                        richieste dell'utente, manipolare il Model di conseguenza e aggiornare la View per riflettere le
+                        modifiche apportate. In Django tuttavia, la logica di controllo è prettamente gestita dalle
+                        View (variante MTV).
+                    </li>
+                </ul>
+            </p>
+            <h2>Model</h2>
+            <p>In Django il Model rappresenta la struttura dei dati e come questi devono essere organizzati, elaborati e
+                archiviati; in sostanza il Model in Django è responsabile di definire la struttura del database,
+                interagendo con esso attraverso il proprio ORM (Object-Relational Mapping).
+            </p>
+            <p>Esempio (modello utenti):
+                <PythonCode code={`
+                    from django.db import models
+                    
+                    class User(models.Model):
+                        first_name = models.CharField(max_length=50)
+                        last_name = models.CharField(max_length=50)
+                        email = models.EmailField(unique=True)
+                        date_joined = models.DateTimeField(auto_now_add=True)
+                        is_active = models.BooleanField(default=True)
+                        
+                        def __str__(self):
+                            return f'User: {self.first_name} {self.last_name}.'
+                `}/>
+            </p>
+            <p>In questo caso è stata creata la classe User(models.Model) che eredita da models.Model le funzionalità
+                di base.
+                Tutti gli altri campi del modello (first_name, last_name, email, date_joined, is_active) rappresentano
+                di fatto gli attributi dell'utente.
+                Infine il metodo __str__ permette di definire come l'oggetto User deve essere rappresentato a livello di
+                leggibilità una volta mandato in stampa l'oggetto stesso o ad esempio quando visualizzato all'interno
+                dell'interfaccia di amministrazione di Django.
+            </p>
+            <p>Definito il modello per far si che questi cambiamenti diventino effettivi a livello di database
+                (creazione tabelle, etc) bisogna eseguire le cosiddette migrazioni, ovvero delle operazioni che
+                sincronizzano il modello con il database.
+                Per creare una migrazione si utilizza il comando:
+                <TerminalCode code={`
+                    python manage.py makemigrations
+                `}/>
+                e per applicarle:
+                <TerminalCode code={`
+                    python manage.py migrate
+                `}/>
+            </p>
+            <p>A questo punto il modello è stato definito e sincronizzato con il database e possiamo quindi interagire
+                con i dati e il database attraverso l'ORM di Django. Se ad esempio volessimo creare un nuovo utente
+                potremmo farlo in questo modo:
+                <PythonCode code={`
+                    from myapp.models import User
+                    
+                    user = User(first_name='John', last_name='Doe', email='example@example.com, is_active=True)
+                    user.save()
+                `}/>
+            </p>
+
+            <h2>Template</h2>
+            <p>In Django (MTV) il Template svolge un ruolo simile alla View del pattern MVC, ovvero si occupa di
+                definire la presentazione dei dati ed il modo in cui devono essere visualizzati nell'interfaccia utente.
+            </p>
+            <p>Di fatto il Template è un file di testo contenente markup HTML insieme a tag e particolari filtri di
+                Django.
+            </p>
+            <p>Il loro utilizzo permette quindi la chiara separazione della logica di presentazione dalla
+                logica di business.
+            </p>
+            <p>Esempio:</p>
+            <PythonCode code={`
+                <\!DOCTYPE html>
+                <html lang="it">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        
+                        <title>Il mio primo sito Django</title>
+                    </head>
+                    <body>
+                        <header>
+                            <h1>Benvenuto su {{ name }}</h1>
+                        </header>
+                        <main>
+                            <h2> Last Articles </h2>
+                            <ul>
+                                {% for article in articles %}
+                                    <li><a href="{{ article.url }}">{{ article.title }}</a></li>
+                                {% endfor %}
+                            </ul>
+                        </main>
+                        <footer>
+                            <p>Copyright &copy; {{ current_year }} - AA</p> 
+                        </footer>   
+                    </body>
+                </html>
+            `}/>
+            <p>Una prima cosa che salta sicuramente all'occhio è:
+                <ul>
+                    <li>l'inserimento di variabili all'interno del template racchiuse tra parentesi graffe
+                        &lbrace;&lbrace; variabile &rbrace;&rbrace; le quali una volta che il template verrà processato
+                        saranno sostituite con il valore corrispondente
+                    </li>
+                    <li>la presenza di una struttura di controllo &lbrace;% for ... in ... %&rbrace;  che
+                        permette di iterare su iterable generando poi di conseguenza codice HTML dinamico, magari con
+                        dati forniti dal Model
+                    </li>
+                </ul>
+                In sostanza quando un utente richiede la pagina, Django processa il template, sostituendo le variabili
+                e valutando le strutture di controllo, per poi restituire l'HTML risultante al browser dell'utente.
+            </p>
+
+            <h2>View</h2>
+            <p>La View si occupa di elaborare la logica di presentazione, interagendo con il Model e il Template per
+                generare la risposta da inviare al client (browser dell'utente).
+            </p>
+            <p>Sostanzialmente le View sono responsabili di ricevere le richieste degli utenti, elaborarle e restituire
+                una risposta appropriata, che nella maggioranza dei casi è un pagani HTML dinamicamente generata grazie
+                all'utilizzo di un Template.
+            </p>
+            <p>Esempio:</p>
+            <PythonCode code={`
+                from django.http import HttpResponse
+                from django.shortcuts import render
+                from .models import Article
+                                    
+                # VIEW function                    
+                def article_list(request):
+                    
+                    # Recupera gli ultimi 10 articoli dal database
+                    last_articles = Article.objects.order_by('-date')[:10]
+                    
+                    # Passa gli articoli al Template
+                    context = {'last_articles': last_articles}
+                    
+                    # Carica il Template e genera una risposta
+                    return render(request, 'articles/article_list.html', context) 
+            `}/>
+            <p>In questo esempio la prima cosa da osservare è che la funzione article_list è la nostra View e come
+                argomento prende un oggetto request contenente le informazioni sulla richiesta HTTP ricevuta dal
+                browser dell'utente.
+            </p>
+            <p>Dopodiché una volta recuperati gli articoli dal database questi vengono passati al template sotto forma
+                di dizionario Python (context).
+            </p>
+            <p>Infine la funzione render() si occupa di caricare il template, popolarlo e generare una risposta HTTP
+                contenente il codice HTML da inviare al client (browser dell'utente).
+            </p>
+            <p>Per far sì che la View sia effettivamente eseguita quando un utente richiede una pagina, è necessario
+                associare la View stessa ad un URL e ciò può essere fatto attraverso il file urls.py: così facendo
+                quando un utente vista l'URL corrispondente Django esegue la View associata restituendo poi la pagina
+                HTML generata al browser dell'utente.
+            </p>
+
+
 
 
 
